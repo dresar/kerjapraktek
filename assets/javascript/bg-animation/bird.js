341530e5ac603744d87caad1851f7457cefbab0e
eka syarif <eka.ckp16799@github.com>
2025-02-21 11:19:01 +0700

    kerjapraktek

---
+++ assets/javascript/bg-animation/bird.js
@@ -0,0 +1,15 @@
+
+/******************** Birds Effect JS ********************/
+VANTA.BIRDS({
+  el: "#birds_bg_effect",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color1: 0xFFFFFF, // Change this to your preferred color1
+  color2: 0x1E1F21, // Change this to your preferred color2
+  backgroundColor: 0x000000, // Black
+})
